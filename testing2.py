18a3897a63801520aecb012e9fb7923cd3567113
Navadeep <prnavadeep.com>
2026-06-26 12:23:13 +0530

    LOAN PROCESSED IS CODED

---
+++ testing2.py
@@ -0,0 +1,19 @@
+print("--" * 60)
+age=int(input("Enter your age: "))
+print("--" * 60)
+if age > 18:
+    print("you are eligible for applying loan")
+    loanamount=int(input("enter the loanamount: "))
+    if loanamount < 50000:
+        print("eligible for loan")
+        pf=int(input("enter your pf: "))
+        if pf > 10000:
+            print ("satisfied with pf")
+            print("your loan amount will be processed")
+        else:
+            print("pf<10000 is not satisfied")
+    else:
+        print("not eligible for loan")
+        
+else:
+    print("you are not eligible for applying the loan")
