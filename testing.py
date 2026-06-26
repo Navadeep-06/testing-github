a8839f253f263de3e595349d6bc9fb83b34aaa32
Navadeep <prnavadeep.com>
2026-06-26 11:59:05 +0530

    added loop in testing git

--- testing.py
+++ testing.py
@@ -1,3 +1,7 @@
-testing how to create repo using terminal
-step -1 testing push changes frm local repo
-step-2 testing pulling changes made in remote repo to local repo
+#testing how to create repo using terminal
+#step -1 testing push changes frm local repo
+#step-2 testing pulling changes made in remote repo to local repo
+
+for a in range (1,11):
+    if a%2==0:
+        print(a+1,end=" " )
